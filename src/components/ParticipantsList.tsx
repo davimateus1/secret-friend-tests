7e187c1593dad0088ac264844ea62fb2bf57e5b6
Davi Mateus <davialmeida@atomstudio.com.br>
2024-03-22 23:08:46 -0300

    fix: list style

--- src/components/ParticipantsList.tsx
+++ src/components/ParticipantsList.tsx
@@ -5,9 +5,16 @@ export const ParticipantsList = () => {
   const participants = useParticipantsList();
 
   return (
-    <UnorderedList>
+    <UnorderedList
+      display="flex"
+      flexDir="column"
+      justifyContent="center"
+      alignItems="center"
+    >
       {participants.map((participant) => (
-        <ListItem key={participant}>{participant}</ListItem>
+        <ListItem key={participant} listStyleType="none">
+          {participant}
+        </ListItem>
       ))}
     </UnorderedList>
   );
